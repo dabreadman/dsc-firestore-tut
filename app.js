62f89069ff1eddefbd615f319cc85e1b34e86ced
dabreadman <tany3@tcd.ie>
2021-03-25 18:56:57 +0000

    Retrieve data from Firestore

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
+// Get list container
+const todo = document.getElementById("todos-list");
 
-    
+// Get form container
+const form = document.getElementById("add-todo-form");
+
+// Get all todos
+db.collection("todos")
+  .get()
+  .then((snapshot) => {
+    snapshot.docs.forEach((doc) => {
+      console.log(doc.data());
+    });
+  });
